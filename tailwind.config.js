f090940958c7831350db1b54eec430e2a73ddc13
Nel <nel.renaudin@gmail.com>
2023-12-12 10:31:00 +0100

    Install Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require("@tailwindcss/forms")],
